a30a8d101786d4ffc92f86195d649c9f0700d850
Chiun Hau You <chiunhau9@gmail.com>
2016-05-21 00:03:27 +0800

    add treee

---
+++ tree/sketch.js
@@ -0,0 +1,41 @@
+var theta = 20;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+	stroke(0);
+	strokeWeight(0.6);
+	noLoop();
+	angleMode(DEGREES);
+	translate(0, windowHeight - 50);
+	translate(windowWidth / 2, 0);
+	line(0, 0, 0, -200);
+	translate(0, -200);
+
+	branch(200);
+
+	translate(0, 200);
+	branch(200);	
+}
+
+
+
+function branch(l) {
+	l *= random(0.6, 0.75);
+	if(l > 4) {
+		push();
+		rotate(random(-theta, theta));
+		line(0, 0, 0, -l);
+		translate(0, -l);
+		branch(l);
+		pop();
+
+		push();
+		rotate(random(-theta, theta));
+		line(0, 0, 0, -l);
+		translate(0, -l);
+		branch(l);
+		pop();
+	}
+
+}
