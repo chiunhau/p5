f5626a8277f338df4304fd34d80fc3bd62caa57a
Chiun Hau You <chiunhau9@gmail.com>
2016-05-26 20:23:24 +0800

    seperate colot

--- touches/sketch.js
+++ touches/sketch.js
@@ -5,9 +5,14 @@ function setup() {
 
 function touchMoved() {
 	console.log(touches);
+	if(touches.length === "1") {
+		background(255, 0, 0);
+	}
+	else if (touches.length === "2") {
+		background(0, 255, 0);
+	}
 }
 
 function touchStart() {
 	console.log(touches);
-}
 }
